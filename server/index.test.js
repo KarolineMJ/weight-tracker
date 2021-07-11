e6002454af56999fc863a0248b05a9ec65900fbb
mariana.carmello <mariana.carmello@creditas.com.br>
2021-07-11 20:42:34 -0300

    create put and delete tests

--- server/index.test.js
+++ server/index.test.js
@@ -32,3 +32,31 @@ describe("GET /measurements", () => {
             .expect(200, body);
     });
 });
+
+app.put("/measurements/:id", function (req, res) {
+    res.status(200).json(body);
+});
+
+describe("PUT /measurements/:id", () => {
+    it("should update a measurement", async () => {
+        const newbody = { measure_date: "2022-01-02", weight: 75 };
+        await request(app)
+            .put("/measurements/" + body.id)
+            .send(newbody)
+            .set("Accept", "application/json")
+            .expect(200);
+    });
+});
+
+app.delete("/measurements/:id", function (req, res) {
+    res.status(200).json(body);
+});
+
+describe("DELETE /measurements/:id", () => {
+    it("should delete a measurement", async () => {
+        await request(app)
+            .delete("/measurements/" + body.id)
+            .set("Accept", "application/json")
+            .expect(200);
+    });
+});
